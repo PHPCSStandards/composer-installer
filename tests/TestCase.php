7891fcda0d29cae1aef7d76df856846c2068bde7
jrfnl <jrfnl@users.noreply.github.com>
2022-01-28 11:15:40 +0100

    TestCase: add helper method for on-the-fly composer.json creation
    This adds a test helper method, which, given an array with a Composer configuration and a target location, will on-the-fly create a `composer.json` file for use in a test.
    
    The method will automatically add two additional settings to the `composer.json` file:
    * It will add a `install-codestandards` script to run the plugin on demand.
    * It will add the `allow-plugins` permission, which is needed for Composer 2.2.
    
    By adding these keys automatically, this kind of "noise" is not needed in the actual test classes.
    
    The method is `static` so as to allow for it to be used in `setUpBeforeClass()`/`tearDownAfterClass()` fixtures.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -82,4 +82,58 @@ abstract class TestCase extends PolyfillTestCase
     {
         return strpos(strtoupper(\PHP_OS), 'WIN') === 0;
     }
+
+    /**
+     * Create a composer.json file based on a given configuration.
+     *
+     * @param array  $config    Composer configuration as an array.
+     * @param string $directory Location to write the resulting `composer.json` file to (without trailing slash).
+     *
+     * @return void
+     *
+     * @throws RuntimeException When either of the passed parameters are of the wrong data type.
+     * @throws RuntimeException When the provided configuration is invalid.
+     * @throws RuntimeException When the configuration could not be written to a file.
+     */
+    protected static function writeComposerJsonFile($config, $directory)
+    {
+        if (is_array($config) === false || $config === array()) {
+            throw new RuntimeException('Configuration must be a non-empty array.');
+        }
+
+        if (is_string($directory) === false || $directory === '') {
+            throw new RuntimeException('Directory must be a non-empty string.');
+        }
+
+        // Inject ability to run the plugin via a script.
+        if (isset($config['scripts']['install-codestandards']) === false) {
+            $config['scripts']['install-codestandards'] = array(
+                'Dealerdirect\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\Plugin::run',
+            );
+        }
+
+        // Inject permission for this plugin to run (Composer 2.2 compat).
+        if (isset($config['config']['allow-plugins']['dealerdirect/phpcodesniffer-composer-installer']) === false) {
+            $config['config']['allow-plugins']['dealerdirect/phpcodesniffer-composer-installer'] = true;
+        }
+
+        /*
+         * Disable TLS when on Windows with Composer 1.x and PHP 5.4.
+         * @link https://github.com/composer/composer/issues/10495
+         */
+        if (static::onWindows() === true && \CLI_PHP_MINOR === '5.4' && substr(\COMPOSER_VERSION, 0, 1) === '1') {
+            $config['config']['disable-tls'] = true;
+        }
+
+        $encoded = json_encode($config, \JSON_UNESCAPED_SLASHES | \JSON_PRETTY_PRINT);
+        if (json_last_error() !== \JSON_ERROR_NONE || $encoded === false) {
+            throw new RuntimeException('Provided configuration can not be encoded to valid JSON');
+        }
+
+        $written = file_put_contents($directory . '/composer.json', $encoded);
+
+        if ($written === false) {
+            throw new RuntimeException('Failed to create the composer.json file in the temp directory for the test');
+        }
+    }
 }
